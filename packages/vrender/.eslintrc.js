35aa2c61f0cdb4b35da00dac2a4ea0db07ee3814
zhouxinyu <zhouxinyu12138@gmail.com>
2023-06-14 21:24:37 +0800

    feat: support export version, closed #25

--- packages/vrender/.eslintrc.js
+++ packages/vrender/.eslintrc.js
@@ -3,5 +3,10 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 module.exports = {
   extends: ['@internal/eslint-config/profile/lib'],
   parserOptions: { tsconfigRootDir: __dirname, project: './tsconfig.eslint.json' },
+  globals: {
+    __DEV__: "readonly",
+    __VERSION__: "readonly",
+    NodeJS: true,
+  },
   // ignorePatterns: [],
 };

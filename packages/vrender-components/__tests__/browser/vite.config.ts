457aba0db6731ef7065f3aa804f092652ed7a84e
kkxxkk2019 <just_star@qq.com>
2023-06-19 10:15:59 +0800

    chore: add __VERSION variable for vite

--- packages/vrender-components/__tests__/browser/vite.config.ts
+++ packages/vrender-components/__tests__/browser/vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
     open: './index.html',
     port: 3333
   },
+  define: {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    __VERSION__: JSON.stringify(require('../../package.json').version)
+  },
   resolve: {
     alias: {
       '@visactor/vrender': path.resolve(__dirname, '../../../vrender/src/index.ts')

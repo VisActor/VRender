e512da1fd7581f966eccb8967112bcba6cc685cd
skie1997 <skie1997@outlook.com>
2023-07-03 18:44:22 +0800

    fix(marker): pickable problem

--- packages/vrender-components/src/marker/base.ts
+++ packages/vrender-components/src/marker/base.ts
@@ -14,12 +14,11 @@ export abstract class Marker<T extends MarkerAttrs> extends AbstractComponent<Re
   protected render() {
     this.removeAllChild();
     const markerVisible = this.attribute.visible ?? true;
-    this.setAttribute('pickable', false);
 
-    const group = createGroup({
-      pickable: false,
-      childrenPickable: false
-    });
+    this.setAttribute('pickable', false);
+    this.setAttribute('childrenPickable', false);
+
+    const group = createGroup({});
     group.name = 'marker-container';
     this.add(group);
     markerVisible && this.renderMarker(group);

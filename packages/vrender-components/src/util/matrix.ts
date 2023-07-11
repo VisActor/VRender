f0940b6cdd59cf23b2d7c5d9fc75de2d323a6970
kkxxkk2019 <just_star@qq.com>
2023-07-11 09:15:19 +0800

    chore: remove gl-matrix

--- packages/vrender-components/src/util/matrix.ts
+++ packages/vrender-components/src/util/matrix.ts
@@ -1,6 +1,42 @@
-import { vec2 } from 'gl-matrix';
+export type Vector2 = [number, number];
 
-export { vec2 };
+/**
+ * Scales a vec2 by a scalar number
+ */
+export function scale(vector: Vector2, scale: number) {
+  return [vector[0] * scale, vector[1] * scale];
+}
+
+/**
+ * Calculates the length of a vec2
+ */
+export function length(vector: Vector2) {
+  const [x, y] = vector;
+  return Math.sqrt(x * x + y * y);
+}
+
+/**
+ * Normalize a vec2
+ */
+export function normalize(vector: Vector2) {
+  const [x, y] = vector;
+  let len = x * x + y * y;
+  if (len > 0) {
+    len = 1 / Math.sqrt(len);
+  }
+  return [vector[0] * len, vector[1] * len];
+}
+
+/**
+ * Get the angle between two 2D vectors
+ */
+export function angle(vector1: Vector2, vector2: Vector2) {
+  const [x1, y1] = vector1;
+  const [x2, y2] = vector2;
+  const mag = Math.sqrt((x1 * x1 + y1 * y1) * (x2 * x2 + y2 * y2));
+  const cosine = mag && (x1 * x2 + y1 * y2) / mag;
+  return Math.acos(Math.min(Math.max(cosine, -1), 1));
+}
 
 /**
  * 向量 v1 到 向量 v2 夹角的方向
@@ -19,7 +55,7 @@ export function direction(v1: number[], v2: number[]): number {
  * @param direct
  */
 export function angleTo(v1: [number, number], v2: [number, number], direct: boolean): number {
-  const ang = vec2.angle(v1, v2);
+  const ang = angle(v1, v2);
   const angleLargeThanPI = direction(v1, v2) >= 0;
   if (direct) {
     if (angleLargeThanPI) {

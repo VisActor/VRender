8378486717d083c2a788d8a03c8f576d9f32d29a
zhouxinyu <zhouxinyu12138@gmail.com>
2023-07-24 16:00:10 +0800

    feat: add setPoptipTheme function

--- packages/vrender-components/src/poptip/register.ts
+++ packages/vrender-components/src/poptip/register.ts
@@ -4,7 +4,7 @@ import { merge } from '@visactor/vutils';
 import { PopTipRenderContribution } from './contribution';
 import { PopTipPlugin, PopTipForClipedTextPlugin } from './poptip-plugin';
 import type { PopTipAttributes } from './type';
-import { theme } from './theme';
+import { DEFAULT_THEME, theme } from './theme';
 
 export const module = new ContainerModule(bind => {
   bind(PopTipRenderContribution).toSelf().inSingletonScope();
@@ -20,4 +20,8 @@ export const module = new ContainerModule(bind => {
 export function loadPoptip(defaultPoptipTheme: PopTipAttributes) {
   merge(theme.poptip, defaultPoptipTheme);
   container.load(module);
+}
+
+export function setPoptipTheme(defaultPoptipTheme: PopTipAttributes) {
+  merge(theme.poptip, DEFAULT_THEME, defaultPoptipTheme);
 }
--- packages/vrender-components/src/poptip/theme.ts
+++ packages/vrender-components/src/poptip/theme.ts
@@ -1,25 +1,28 @@
+import { merge } from '@visactor/vutils';
 import type { PopTipAttributes } from './type';
 
-export const theme: { poptip: PopTipAttributes } = {
-  poptip: {
+export const DEFAULT_THEME = {
+  visible: true,
+  position: 'top',
+  titleStyle: {
+    fontSize: 16,
+    fill: '#08979c'
+  },
+  contentStyle: {
+    fontSize: 12,
+    fill: 'green'
+  },
+  panel: {
     visible: true,
-    position: 'top',
-    titleStyle: {
-      fontSize: 16,
-      fill: '#08979c'
-    },
-    contentStyle: {
-      fontSize: 12,
-      fill: 'green'
-    },
-    panel: {
-      visible: true,
 
-      fill: '#e6fffb',
+    fill: '#e6fffb',
 
-      stroke: '#87e8de',
-      lineWidth: 1,
-      cornerRadius: 4
-    }
+    stroke: '#87e8de',
+    lineWidth: 1,
+    cornerRadius: 4
   }
+};
+
+export const theme: { poptip: PopTipAttributes } = {
+  poptip: merge({}, DEFAULT_THEME)
 };

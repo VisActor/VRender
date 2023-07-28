bc4e9c7bf1774ecbf7f2d26827805d8ea33595ad
pairone <1063258712@qq.com>
2023-07-28 14:48:15 +0800

    feat: change textAlign & textBaseline type

--- packages/vrender-components/src/label/arc.ts
+++ packages/vrender-components/src/label/arc.ts
@@ -3,7 +3,7 @@ import { merge } from '@visactor/vutils';
 import { LabelBase } from './base';
 import type { ArcLabelAttrs, IPoint, Quadrant, TextAlign } from './type';
 import type { BaseLabelAttrs } from './type';
-import type { ITextGraphicAttribute } from '@visactor/vrender';
+import type { ITextGraphicAttribute, TextAlignType, TextBaselineType } from '@visactor/vrender';
 import { isValidNumber, isNil, isLess, isGreater, isNumberClose as isClose } from '@visactor/vutils';
 import {
   circlePoint,
@@ -45,8 +45,8 @@ export class ArcInfo {
   radian: number;
   middleAngle: number;
   k: number;
-  textAlign: string;
-  textBaseline: string;
+  textAlign: TextAlignType;
+  textBaseline: TextBaselineType;
   angle: number;
 
   constructor(

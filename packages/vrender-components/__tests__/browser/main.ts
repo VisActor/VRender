59bb337e7e869f27cc81e8e4b49f18089a83b51c
kkxxkk2019 <just_star@qq.com>
2023-08-06 18:04:42 +0800

    chore: add line axis containerAlign demo

--- packages/vrender-components/__tests__/browser/main.ts
+++ packages/vrender-components/__tests__/browser/main.ts
@@ -3,6 +3,10 @@ import './style.css';
 const LOCAL_STORAGE_KEY = 'VRENDER_COMPONENTS_DEMOS';
 const specs = [
   {
+    path: 'cartesian-axis-label-align',
+    name: '轴标签整体对齐'
+  },
+  {
     path: 'axis-overlap',
     name: '轴标签防重叠'
   },
@@ -223,7 +227,8 @@ const handleClick = (e: { target: any }, isInit?: boolean) => {
         module.run?.();
       })
       .catch(err => {
-        // console.log(err);
+        // eslint-disable-next-line no-console
+        console.log(err);
       });
   }
 };

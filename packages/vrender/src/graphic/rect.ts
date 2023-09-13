a5cdb19ec9dfdf48ae8522a29c4fed93500f2dd6
zhouxinyu <zhouxinyu12138@gmail.com>
2023-09-13 14:57:35 +0800

    feat: export no work animate attr, closed #483

--- packages/vrender/src/graphic/rect.ts
+++ packages/vrender/src/graphic/rect.ts
@@ -1,5 +1,5 @@
 import type { AABBBounds, OBBBounds } from '@visactor/vutils';
-import { Graphic, GRAPHIC_UPDATE_TAG_KEY } from './graphic';
+import { Graphic, GRAPHIC_UPDATE_TAG_KEY, NOWORK_ANIMATE_KEY } from './graphic';
 import type { GraphicType, IRect, IRectGraphicAttribute } from '../interface';
 import { CustomPath2D } from '../common/custom-path2d';
 import { parsePadding } from '../common/utils';
@@ -11,6 +11,8 @@ const RECT_UPDATE_TAG_KEY = ['width', 'height', 'cornerRadius', ...GRAPHIC_UPDAT
 
 export class Rect extends Graphic<IRectGraphicAttribute> implements IRect {
   type: GraphicType = 'rect';
+
+  static NOWORK_ANIMATE_KEY = NOWORK_ANIMATE_KEY;
 
   constructor(params: IRectGraphicAttribute) {
     super(params);

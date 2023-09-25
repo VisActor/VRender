5e50d2240ed628e892deb59c95adac98a39bacfa
skie1997 <skie1997@outlook.com>
2023-09-25 14:06:44 +0800

    chore: unit test

--- packages/vrender-components/src/data-zoom/type.ts
+++ packages/vrender-components/src/data-zoom/type.ts
@@ -9,6 +9,7 @@ import type {
 import type { IPointLike } from '@visactor/vutils';
 import type { OrientType } from '../interface';
 import type { TagAttributes } from '../tag';
+import type { IDelayType } from '../common/type';
 
 export interface DataZoomAttributes extends IGroupGraphicAttribute {
   /**
@@ -187,5 +188,3 @@ export interface DataZoomAttributes extends IGroupGraphicAttribute {
   previewPointsY1?: (datum: any) => number;
   updateStateCallback?: (start: number, end: number) => any;
 }
-
-export type IDelayType = 'debounce' | 'throttle';

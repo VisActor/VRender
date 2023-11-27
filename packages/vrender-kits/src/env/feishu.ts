476986b345f8550b9fc827877803c65a5a314dd8
zhouxinyu <zhouxinyu12138@gmail.com>
2023-11-27 20:37:24 +0800

    fix: fix issue with vrender-kits tree shaking

--- packages/vrender-kits/src/env/feishu.ts
+++ packages/vrender-kits/src/env/feishu.ts
@@ -1,8 +1,18 @@
-import { container, type Container } from '@visactor/vrender-core';
-import { feishuEnvModule } from './contributions/module';
+import { container, ContainerModule, type Container, EnvContribution } from '@visactor/vrender-core';
 import { feishuCanvasModule } from '../canvas/contributions/feishu/modules';
 import { feishuWindowModule } from '../window/contributions/feishu-contribution';
 import { loadMathPicker } from '../picker/math-module';
+import { FeishuEnvContribution } from './contributions/feishu-contribution';
+
+let isFeishuBound = false;
+export const feishuEnvModule = new ContainerModule(bind => {
+  // feishu
+  if (!isFeishuBound) {
+    isFeishuBound = true;
+    bind(FeishuEnvContribution).toSelf().inSingletonScope();
+    bind(EnvContribution).toService(FeishuEnvContribution);
+  }
+});
 
 let loaded = false;
 export function loadFeishuEnv(container: Container, loadPicker: boolean = true) {
--- packages/vrender-kits/src/env/node.ts
+++ packages/vrender-kits/src/env/node.ts
@@ -1,8 +1,19 @@
-import { container, type Container } from '@visactor/vrender-core';
-import { loadMathPicker } from '../picker/math-module';
-import { nodeEnvModule } from './contributions/module';
+import { container, ContainerModule, type Container, EnvContribution } from '@visactor/vrender-core';
+// import { loadMathPicker } from '../picker/math-module';
+// import { nodeEnvModule } from './contributions/module';
 import { nodeCanvasModule } from '../canvas/contributions/node/modules';
 import { nodeWindowModule } from '../window/contributions/node-contribution';
+import { NodeEnvContribution } from './contributions/node-contribution';
+
+let isNodeBound = false;
+export const nodeEnvModule = new ContainerModule(bind => {
+  // node
+  if (!isNodeBound) {
+    isNodeBound = true;
+    bind(NodeEnvContribution).toSelf().inSingletonScope();
+    bind(EnvContribution).toService(NodeEnvContribution);
+  }
+});
 
 let loaded = false;
 export function loadNodeEnv(container: Container, loadPicker: boolean = true) {
@@ -11,7 +22,7 @@ export function loadNodeEnv(container: Container, loadPicker: boolean = true) {
     container.load(nodeEnvModule);
     container.load(nodeCanvasModule);
     container.load(nodeWindowModule);
-    loadPicker && loadMathPicker(container);
+    // loadPicker && loadMathPicker(container);
   }
 }
 

50622f340c3e1bfe0fe0c32e00ed1586f73a7bf4
kkxxkk2019 <just_star@qq.com>
2024-01-09 20:15:47 +0800

    fix: measureTextSize method should consider the fontFamily set by stage.setTheme

--- packages/vrender-components/src/indicator/indicator.ts
+++ packages/vrender-components/src/indicator/indicator.ts
@@ -193,7 +193,8 @@ export class Indicator extends AbstractComponent<Required<IndicatorAttributes>> 
     }
     const originWidth = measureTextSize(
       (indicatorItemSpec.style?.text ?? '') as string | number | number[] | string[],
-      (indicatorItemSpec.style ?? {}) as Partial<ITextGraphicAttribute>
+      (indicatorItemSpec.style ?? {}) as Partial<ITextGraphicAttribute>,
+      this.stage?.getTheme().text.fontFamily
     ).width;
     if (originWidth > 0) {
       const ratio = (limit * (indicatorItemSpec.fitPercent ?? 0.5)) / originWidth;

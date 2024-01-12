56afed3129494932fbfd14627051b569a71dc288
zhouxinyu <zhouxinyu12138@gmail.com>
2024-01-12 18:27:43 +0800

    fix: fix issue with shadow bounds

--- packages/vrender-core/src/graphic/shadow-root.ts
+++ packages/vrender-core/src/graphic/shadow-root.ts
@@ -57,15 +57,11 @@ export class ShadowRoot extends Group {
   // 计算AABBBounds不受影响
   // 计算globalAABBBounds
   protected tryUpdateGlobalAABBBounds(): AABBBounds {
+    const b = this.AABBBounds;
     if (!this._globalAABBBounds) {
-      this._globalAABBBounds = this._AABBBounds.clone();
+      this._globalAABBBounds = b.clone();
     } else {
-      this._globalAABBBounds.setValue(
-        this._AABBBounds.x1,
-        this._AABBBounds.y1,
-        this._AABBBounds.x2,
-        this._AABBBounds.y2
-      );
+      this._globalAABBBounds.setValue(b.x1, b.y1, b.x2, b.y2);
     }
     // 使用shadowHost的grloalAABBBounds
     // todo: 考虑是否需要性能优化

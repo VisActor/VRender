47c3416d63c28dca88291f500c09b5b108571c6c
zhouxinyu <zhouxinyu12138@gmail.com>
2024-01-24 15:27:11 +0800

    feat: support backgroundCornerRadius

--- packages/vrender/__tests__/browser/src/pages/text.ts
+++ packages/vrender/__tests__/browser/src/pages/text.ts
@@ -100,7 +100,7 @@ export const page = () => {
       },
       background: 'red',
       backgroundCornerRadius: 10,
-      text: ['这是一行文字'],
+      text: '这',
       fontSize: 36,
       textBaseline: 'top'
     })

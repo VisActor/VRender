418ff21d4f0c93c659fb2c4d8b8670aca89857d7
xile611 <dingling112@gmail.com>
2024-01-24 15:44:20 +0800

    feat: support `autoEllipsisStrategy` of legend item

--- packages/vrender-components/src/legend/discrete/type.ts
+++ packages/vrender-components/src/legend/discrete/type.ts
@@ -79,10 +79,23 @@ export type LegendItem = {
     /** shape 同后面 label 的间距 */
     space?: number;
   } & BaseGraphicAttributes<Partial<ISymbolGraphicAttribute> | StyleCallback<Partial<ISymbolGraphicAttribute>>>;
+
+  /**
+   * 当label+ value同时存在的时候，自动省略的策略
+   * 'labelFirst' - 尽量保证完整展示`label`
+   * 'valueFirst' - 尽量保证完整展示`value`
+   * 'none' - 按照`widthRatio`展示label 和 value
+   */
+  autoEllipsisStrategy?: 'labelFirst' | 'valueFirst' | 'none';
+
   /**
    * 图例项的 label 文本配置
    */
   label?: {
+    /**
+     * 当 label + value 同时展示，切超长的时候，label的宽度占比
+     */
+    widthRatio?: number;
     /**
      * 图例项 label 同后面 value 的间距
      */
@@ -96,6 +109,10 @@ export type LegendItem = {
    * 图例项 value 配置
    */
   value?: {
+    /**
+     * 当 label + value 同时展示，切超长的时候，label的宽度占比
+     */
+    widthRatio?: number;
     /** value 同后面元素的间距 */
     space?: number;
     /**

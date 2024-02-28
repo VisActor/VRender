820c631686e9c3e9da1eaf0fa764dbcdd643f284
zhouxinyu <zhouxinyu12138@gmail.com>
2024-02-28 12:19:42 +0800

    feat: richtext support inherit attribute, closed #946

--- packages/vrender-core/src/graphic/config.ts
+++ packages/vrender-core/src/graphic/config.ts
@@ -310,6 +310,7 @@ export const DefaultTextAttribute: Required<ITextGraphicAttribute> = {
 
 export const DefaultRichTextAttribute: Required<IRichTextGraphicAttribute> = {
   ...DefaultAttribute,
+  ...DefaultTextStyle,
   width: 300,
   height: 300,
   ellipsis: true,

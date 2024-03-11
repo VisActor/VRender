aae4503771d2e1866408a8a28a0e6fc5ebc73d65
xiaoluoHe <sevnape@outlook.com>
2024-03-11 18:34:53 +0800

    feat: optimize autoLimit when axis label is rotated

--- packages/vrender-components/src/axis/overlap/auto-limit.ts
+++ packages/vrender-components/src/axis/overlap/auto-limit.ts
@@ -3,47 +3,72 @@
  */
 import type { IText } from '@visactor/vrender-core';
 import { isEmpty, isNil, isValidNumber } from '@visactor/vutils';
+import { borderPoint } from './util';
 
 type LimitConfig = {
   orient: string;
   limitLength: number;
+  axisLength: number;
   verticalLimitLength?: number;
   ellipsis?: string;
 };
 
 export function autoLimit(labels: IText[], config: LimitConfig) {
-  const { limitLength, verticalLimitLength, ellipsis = '...', orient } = config;
-
+  const { limitLength, verticalLimitLength, ellipsis = '...', orient, axisLength } = config;
   if (isEmpty(labels) || !isValidNumber(limitLength)) {
     return;
   }
 
   labels.forEach(label => {
     const angle = label.attribute.angle;
-    const isRotated = !isNil(angle) && angle !== 0;
 
-    if (
-      (orient === 'top' || orient === 'bottom') &&
-      ((isRotated && Math.floor(label.AABBBounds.height()) <= limitLength) ||
-        (!isRotated && Math.floor(label.AABBBounds.width()) <= verticalLimitLength))
-    ) {
-      return;
+    const hasAngle = !isNil(angle);
+    const isHorizontal = !hasAngle || angle === 0 || angle === Math.PI;
+    const isVertical = hasAngle && (angle === Math.PI / 2 || angle === (Math.PI * 2) / 3);
+    const isX = orient === 'top' || orient === 'bottom';
+
+    if (isX) {
+      if (isVertical && Math.floor(label.AABBBounds.height()) <= limitLength) {
+        return;
+      }
+      if (isHorizontal && Math.floor(label.AABBBounds.width()) <= verticalLimitLength) {
+        return;
+      }
     }
+
     const direction = label.attribute.direction;
-    if (
-      (orient === 'left' || orient === 'right') &&
-      ((direction === 'vertical' && Math.floor(label.AABBBounds.height()) <= verticalLimitLength) ||
-        (direction !== 'vertical' && Math.floor(label.AABBBounds.width()) <= limitLength))
-    ) {
-      return;
+    if (!isX) {
+      if (direction === 'vertical' && Math.floor(label.AABBBounds.height()) <= verticalLimitLength) {
+        return;
+      }
+
+      if (direction !== 'vertical') {
+        if (isHorizontal && Math.floor(label.AABBBounds.width()) <= limitLength) {
+          return;
+        }
+        if (isVertical && Math.floor(label.AABBBounds.height()) <= verticalLimitLength) {
+          return;
+        }
+      }
     }
 
     // 如果水平并且文本未发生旋转，则不配置 maxLineWidth
     let limitLabelLength = null;
 
-    if (isRotated) {
-      limitLabelLength = Math.abs(limitLength / Math.sin(angle));
-    } else if (orient === 'top' || orient === 'bottom') {
+    if (!isHorizontal && !isVertical) {
+      const { x1: x, y1: y } = label.AABBBounds;
+
+      const width = isX ? axisLength : limitLength;
+      const height = isX ? limitLength : axisLength;
+
+      const intersection = borderPoint({ width, height, left: 0, top: 0 }, { x, y }, label.attribute.angle);
+      if (intersection) {
+        const { x: _x, y: _y } = intersection;
+        limitLabelLength = Math.floor(Math.sqrt((_x - x) ** 2 + (_y - y) ** 2));
+      } else {
+        limitLabelLength = Math.abs(limitLength / Math.sin(angle));
+      }
+    } else if (isX) {
       limitLabelLength = verticalLimitLength;
     } else {
       limitLabelLength = direction === 'vertical' ? verticalLimitLength : limitLength;

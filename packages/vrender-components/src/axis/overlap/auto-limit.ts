59e283886a808105b0872d1b0157947469d97fb2
xile611 <dingling112@gmail.com>
2024-03-22 11:52:47 +0800

    fix: fix limit length of different angles

--- packages/vrender-components/src/axis/overlap/auto-limit.ts
+++ packages/vrender-components/src/axis/overlap/auto-limit.ts
@@ -60,22 +60,24 @@ export function autoLimit(labels: IText[], config: LimitConfig) {
     if (!isHorizontal && !isVertical) {
       if (isX) {
         const { x1, x2 } = label.AABBBounds;
+        const tan = sin / cos;
 
-        if (cos > 0 && x1 <= axisLength && (cos * limitLength) / Math.abs(sin) + x1 > axisLength) {
-          limitLabelLength = (axisLength - x1) / cos;
-        } else if (cos < 0 && x2 >= 0 && (cos * limitLength) / Math.abs(sin) + x2 < 0) {
-          limitLabelLength = -x2 / cos;
+        if (tan > 0 && x1 <= axisLength && limitLength / tan + x1 > axisLength) {
+          limitLabelLength = (axisLength - x1) / Math.abs(cos);
+        } else if (tan < 0 && x2 >= 0 && limitLength / tan + x2 < 0) {
+          limitLabelLength = x2 / Math.abs(cos);
         } else {
           limitLabelLength = Math.abs(limitLength / sin);
         }
       } else {
         // y轴暂时不限制在平行于坐标轴的矩形内，后续可以考虑通过配置开启
         // const { y1, y2 } = label.AABBBounds;
+        // const tan = sin / cos;
 
-        // if (sin > 0 && y2 >= 0 && y2 - (sin * limitLength) / Math.abs(cos) < 0) {
-        //   limitLabelLength = y2 / sin;
-        // } else if (sin < 0 && y1 <= axisLength && y1 - (sin * limitLength) / Math.abs(cos) > axisLength) {
-        //   limitLabelLength = -(axisLength - y1) / sin;
+        // if (tan > 0 && y2 >= 0 && y2 - tan * limitLength < 0) {
+        //   limitLabelLength = y2 / Math.abs(sin);
+        // } else if (tan < 0 && y1 <= axisLength && y1 - tan * limitLength > axisLength) {
+        //   limitLabelLength = (axisLength - y1) / Math.abs(sin);
         // } else {
         // }
         limitLabelLength = Math.abs(limitLength / cos);

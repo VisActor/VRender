6fa9a3c8ee08a970b7a565621d316e1e5d9f0a2d
jiangtao.yang <jiangtao.young@qq.com>
2024-04-16 21:55:51 +0800

    refactor: replace wrap text with text

--- packages/vrender-components/src/poptip/register.ts
+++ packages/vrender-components/src/poptip/register.ts
@@ -2,11 +2,11 @@ import { container } from '@visactor/vrender-core';
 import { merge } from '@visactor/vutils';
 import type { PopTipAttributes } from './type';
 import { DEFAULT_THEME, theme } from './theme';
-import { registerGroup, registerRect, registerSymbol, registerWrapText } from '@visactor/vrender-kits';
+import { registerGroup, registerRect, registerSymbol, registerText } from '@visactor/vrender-kits';
 
 export function loadPoptipComponent() {
   registerGroup();
-  registerWrapText();
+  registerText();
   registerSymbol();
   registerRect();
 }

43f155a8dddcd6da928d4dfad759c0073d60b14e
xiaoluoHe <sevnape@outlook.com>
2024-09-02 15:49:29 +0800

    fix: line label data

--- packages/vrender-components/src/label/line.ts
+++ packages/vrender-components/src/label/line.ts
@@ -1,6 +1,6 @@
 import type { IBoundsLike, IPointLike } from '@visactor/vutils';
-import { merge } from '@visactor/vutils';
-import type { ILine } from '@visactor/vrender-core';
+import { isArray, merge } from '@visactor/vutils';
+import type { ILine, IRichText, IText } from '@visactor/vrender-core';
 import type { PointLocationCfg } from '../core/type';
 import type { LineLabelAttrs } from './type';
 import { LabelBase } from './base';
@@ -23,6 +23,34 @@ export class LineLabel extends LabelBase<LineLabelAttrs> {
   constructor(attributes: LineLabelAttrs, options?: ComponentOptions) {
     const { data, ...restAttributes } = attributes;
     super(options?.skipDefault ? attributes : { data, ...merge({}, LineLabel.defaultAttributes, restAttributes) });
+  }
+
+  protected _initText(data: any[] = []): (IText | IRichText)[] {
+    const { textStyle = {} } = this.attribute;
+    const labels = [];
+    for (let i = 0; i < data.length; i++) {
+      const textData = data[i];
+      const baseMark = this.getRelatedGraphic(textData);
+      if (!baseMark) {
+        continue;
+      }
+      const position = this.attribute.position;
+      const pointData = textData[position === 'end' ? textData.length - 1 : 0];
+      const labelAttribute = {
+        fill: this._isCollectionBase
+          ? isArray(baseMark.attribute.stroke)
+            ? baseMark.attribute.stroke.find(entry => !!entry && entry !== true)
+            : baseMark.attribute.stroke
+          : baseMark.attribute.fill,
+        id: textData.id,
+        ...textStyle,
+        ...pointData
+      };
+      const text = this._createLabelText(labelAttribute);
+      labels.push(text);
+    }
+
+    return labels;
   }
 
   protected getGraphicBounds(graphic: ILine, point: Partial<PointLocationCfg> = {}, position = 'end') {

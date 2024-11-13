00a1fa087c4042251508afbb7af9282639cc82c4
zhouxinyu <zhouxinyu12138@gmail.com>
2024-11-13 18:05:08 +0800

    feat: support easeInOutQuad easeOutElastic  easeInOutElastic

--- packages/vrender-core/src/animate/easing.ts
+++ packages/vrender-core/src/animate/easing.ts
@@ -222,4 +222,29 @@ export class Easing {
   static elasticIn = Easing.getElasticIn(1, 0.3);
   static elasticOut = Easing.getElasticOut(1, 0.3);
   static elasticInOut = Easing.getElasticInOut(1, 0.3 * 1.5);
+
+  static easeInOutQuad = (t: number) => {
+    if ((t /= 0.5) < 1) {
+      return 0.5 * Math.pow(t, 2);
+    }
+    return -0.5 * ((t -= 2) * t - 2);
+  };
+
+  static easeOutElastic = (x: number) => {
+    const c4 = (2 * Math.PI) / 3;
+
+    return x === 0 ? 0 : x === 1 ? 1 : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
+  };
+
+  static easeInOutElastic = (x: number) => {
+    const c5 = (2 * Math.PI) / 4.5;
+
+    return x === 0
+      ? 0
+      : x === 1
+      ? 1
+      : x < 0.5
+      ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
+      : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
+  };
 }

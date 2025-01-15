46fba1348bdc73a3cee1a1b25c2450bf9895dfbd
xiaoluoHe <sevnape@outlook.com>
2025-01-15 14:30:17 +0800

    feat: support transparent foreground and background color for label `smartInvert`

--- packages/vrender-components/src/label/base.ts
+++ packages/vrender-components/src/label/base.ts
@@ -1022,6 +1022,12 @@ export class LabelBase<T extends BaseLabelAttrs> extends AbstractComponent<T> {
        * */
       let backgroundColor = baseMark.attribute.fill as IColor;
       let foregroundColor = label.attribute.fill as IColor;
+      const { fillOpacity: backgroundFillOpacity = 1, opacity: backgroundOpacity = 1 } = baseMark.attribute;
+      const {
+        fillOpacity: foregroundFillOpacity = 1,
+        opacity: foregroundOpacity = 1,
+        strokeOpacity: foregroundStrokeOpacity = 1
+      } = label.attribute;
 
       if (isObject(backgroundColor) && backgroundColor.gradient) {
         const firstStopColor = (backgroundColor as ILinearGradient).stops?.[0]?.color;
@@ -1035,6 +1041,8 @@ export class LabelBase<T extends BaseLabelAttrs> extends AbstractComponent<T> {
       const invertColor = labelSmartInvert(
         foregroundColor,
         backgroundColor,
+        foregroundFillOpacity * foregroundOpacity,
+        backgroundFillOpacity * backgroundOpacity,
         textType,
         contrastRatiosThreshold,
         alternativeColors,
@@ -1069,6 +1077,8 @@ export class LabelBase<T extends BaseLabelAttrs> extends AbstractComponent<T> {
             fill: labelSmartInvert(
               label.attribute.fill as IColor,
               label.attribute.stroke as IColor,
+              foregroundFillOpacity * foregroundOpacity,
+              foregroundStrokeOpacity * foregroundOpacity,
               textType,
               contrastRatiosThreshold,
               alternativeColors,

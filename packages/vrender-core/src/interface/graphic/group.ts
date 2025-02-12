fd52ff4ce6bbb5b11059f1bc7ed4e7b18c3b9ebe
xile611 <dingling112@gmail.com>
2025-02-12 15:59:23 +0800

    docs: update desc of graphics

--- packages/vrender-core/src/interface/graphic/group.ts
+++ packages/vrender-core/src/interface/graphic/group.ts
@@ -4,20 +4,63 @@ import type { INode } from '../node-tree';
 import type { GraphicAttributeMap } from './creator';
 
 export type IGroupAttribute = {
+  /**
+   * 路径，用于绘制路径图形或者设置裁剪图形
+   */
   path: IGraphic[];
+  /**
+   * 宽度
+   */
   width: number;
+  /**
+   * 高度
+   */
   height: number;
+  /**
+   * 圆角半径
+   */
   cornerRadius: number | number[];
+  /**
+   * 圆角类型，
+   * 'round' - 圆弧
+   * 'bevel' - 斜角
+   */
   cornerType: 'round' | 'bevel';
+  /**
+   * 是否剪裁
+   */
   clip: boolean;
+  /**
+   * 所有的子节点是否可见
+   */
   visibleAll: boolean;
+  /**
+   * 布局方式
+   */
   display?: 'relative' | 'inner-block' | 'flex';
+  /**
+   * flex布局的方向
+   */
   flexDirection?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  /**
+   * flex布局的换行
+   */
   flexWrap?: 'nowrap' | 'wrap';
+  /**
+   * flex布局中，子元素在主轴上的对齐方式
+   */
   justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around';
+  /**
+   * flex布局中，子元素在交叉轴上的对齐方式
+   */
   alignItems?: 'flex-start' | 'flex-end' | 'center';
+  /**
+   * flex布局中，多行内容的垂直对齐方式
+   */
   alignContent?: 'flex-start' | 'center' | 'space-between' | 'space-around';
-  // 基准的透明度，用于控制group下面整体图元的透明度
+  /**
+   * 基准的透明度，用于控制group下面整体图元的透明度
+   */
   baseOpacity?: number;
 };
 

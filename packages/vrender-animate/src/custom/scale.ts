352dbdef85ad54d5addace651963e51eeb2d3ebe
zhouxinyu <zhouxinyu12138@gmail.com>
2025-03-27 15:07:40 +0800

    fix: fix issue with getAttributes, optmize performance for scale and executor

--- packages/vrender-animate/src/custom/scale.ts
+++ packages/vrender-animate/src/custom/scale.ts
@@ -13,10 +13,12 @@ export class ScaleIn extends ACustomAnimate<Record<string, number>> {
     super(from, to, duration, easing, params);
   }
 
+  declare _updateFunction: (ratio: number) => void;
+
   onBind(): void {
     // 用于入场的时候设置属性（因为有动画的时候VChart不会再设置属性了）
     if (this.params?.diffAttrs) {
-      this.target.setAttributes(this.params.diffAttrs);
+      Object.assign(this.target.attribute, this.params.diffAttrs);
     }
     let from: Record<string, number>;
     let to: Record<string, number>;
@@ -27,10 +29,14 @@ export class ScaleIn extends ACustomAnimate<Record<string, number>> {
       case 'x':
         from = { scaleX: fromAttrs.scaleX ?? 0 };
         to = { scaleX: attrs?.scaleX ?? 1 };
+        this.propKeys = ['scaleX'];
+        this._updateFunction = this.updateX;
         break;
       case 'y':
         from = { scaleY: fromAttrs.scaleY ?? 0 };
         to = { scaleY: attrs?.scaleY ?? 1 };
+        this.propKeys = ['scaleY'];
+        this._updateFunction = this.updateY;
         break;
       case 'xy':
       default:
@@ -39,24 +45,41 @@ export class ScaleIn extends ACustomAnimate<Record<string, number>> {
           scaleX: attrs?.scaleX ?? 1,
           scaleY: attrs?.scaleY ?? 1
         };
+        this.propKeys = ['scaleX', 'scaleY'];
+        this._updateFunction = this.updateXY;
     }
+
     this.props = to;
-    this.propKeys = Object.keys(to);
-    this.animate.reSyncProps();
+    // 性能消耗，不用reSyncProps
+    // this.animate.reSyncProps();
     this.from = from;
     this.to = to;
-    this.target.setAttributes(from);
+    // 性能优化，不需要setAttributes
+    Object.assign(this.target.attribute, from);
+    this.target.addUpdatePositionTag();
+    this.target.addUpdateBoundTag();
   }
 
   onEnd(cb?: (animate: IAnimate, step: IStep) => void): void {
     super.onEnd(cb);
   }
 
+  updateX(ratio: number): void {
+    this.target.attribute.scaleX = this.from.scaleX + (this.to.scaleX - this.from.scaleX) * ratio;
+  }
+
+  updateY(ratio: number): void {
+    this.target.attribute.scaleY = this.from.scaleY + (this.to.scaleY - this.from.scaleY) * ratio;
+  }
+
+  updateXY(ratio: number): void {
+    this.updateX(ratio);
+    this.updateY(ratio);
+  }
+
   onUpdate(end: boolean, ratio: number, out: Record<string, any>): void {
-    this.propKeys.forEach(key => {
-      out[key] = this.from[key] + (this.to[key] - this.from[key]) * ratio;
-    });
-    this.target.setAttributes(out);
+    this._updateFunction(ratio);
+    this.target.addUpdatePositionTag();
   }
 }
 

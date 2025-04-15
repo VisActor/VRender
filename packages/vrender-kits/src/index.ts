e78e4f5a4a7bfdfc0162d00de2b93efdad0283bb
Xuefei Li <lixuef1313@163.com>
2025-04-15 15:59:44 +0800

    Merge pull request #1808 from VisActor/feat/export-register-gif-image
    feat: add export gif image

--- packages/vrender-kits/src/index.ts
+++ packages/vrender-kits/src/index.ts
@@ -78,6 +78,7 @@ export * from './register/register-shadowRoot';
 export * from './register/register-symbol';
 export * from './register/register-text';
 export * from './register/register-wraptext';
+export * from './register/register-gif';
 export * from './tools/dynamicTexture/effect';
 // export const canvasModuleLoader = _canvasModuleLoader;
 // export { nodeLoader } from './node-bind'; // nodeLoader只在node入口暴露

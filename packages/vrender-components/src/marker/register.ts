b910807450aae5b4f0b1c88d1ed0d7a467406053
skie1997 <skie1997@outlook.com>
2025-05-16 17:24:32 +0800

    fix: solve animate problem

--- packages/vrender-components/src/marker/register.ts
+++ packages/vrender-components/src/marker/register.ts
@@ -8,29 +8,35 @@ import {
 } from '@visactor/vrender-kits';
 import { loadTagComponent } from '../tag/register';
 import { loadArcSegmentComponent, loadSegmentComponent } from '../segment/register';
+import { registerAnimate } from '@visactor/vrender-animate';
 function loadBaseMarker() {
   registerGroup();
   loadTagComponent();
+  registerAnimate();
 }
 
 export function loadMarkLineComponent() {
   loadBaseMarker();
   loadSegmentComponent();
+  registerAnimate();
 }
 
 export function loadMarkArcLineComponent() {
   loadBaseMarker();
   loadArcSegmentComponent();
+  registerAnimate();
 }
 
 export function loadMarkAreaComponent() {
   loadBaseMarker();
   registerPolygon();
+  registerAnimate();
 }
 
 export function loadMarkArcAreaComponent() {
   loadBaseMarker();
   registerArc();
+  registerAnimate();
 }
 
 export function loadMarkPointComponent() {
@@ -40,4 +46,5 @@ export function loadMarkPointComponent() {
   registerSymbol();
   registerImage();
   registerLine();
+  registerAnimate();
 }

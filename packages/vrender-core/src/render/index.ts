807f4bc58e108b91ace93efa3b884c33eb317d85
zhouxinyu <zhouxinyu12138@gmail.com>
2025-09-16 17:49:09 +0800

    fix: export BeforeRenderConstribution and RenderService

--- packages/vrender-core/src/render/index.ts
+++ packages/vrender-core/src/render/index.ts
@@ -1,5 +1,6 @@
 // export { RenderService, IDrawContribution, IRenderService, IRenderServiceDrawParams } from './render-service';
 // export { GraphicRender, IGraphicRender } from './contributions/render/graphic-render';
 export * from './render-service';
+export * from './constants';
 export * from './contributions/render';
 export * from './contributions/render/contributions/constants';
